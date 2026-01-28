2221bf9ad2a50f09768a45848a4ec13451d29007
mek0124 <mek0124@proton.me>
2026-01-28 15:09:27 -0600

    updated

---
+++ app/pages/__init__.py
@@ -0,0 +1,4 @@
+from .dashboard import Dashboard
+from .profile import Profile
+
+__all__ = ["Dashboard", "Profile"]
---
+++ app/pages/components/__init__.py
@@ -0,0 +1,4 @@
+from .warning_strip import WarningStrip
+from .main_display import MainDisplay
+
+__all__ = ["WarningStrip", "MainDisplay"]
